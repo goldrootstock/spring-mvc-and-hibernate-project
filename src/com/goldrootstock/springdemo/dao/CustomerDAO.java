2571dc4bc6e52c156d963869d2a2b38b774483dd
juwonkim <goldrootstock@gmail.com>
2021-10-01 13:14:41 +0900

    Developing Hibernate DAO

---
+++ src/com/goldrootstock/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.goldrootstock.springdemo.dao;
+
+import java.util.List;
+
+import com.goldrootstock.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
