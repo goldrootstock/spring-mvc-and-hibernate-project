5744a859cabba7694c400ec0598818d7657e3de3
juwonkim <goldrootstock@gmail.com>
2021-10-04 14:35:12 +0900

    Added AOP Logging

---
+++ src/com/goldrootstock/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,73 @@
+package com.goldrootstock.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+	
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	// setup pointcut declarations
+	@Pointcut("execution(* com.goldrootstock.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	// do the same for service and dao
+	@Pointcut("execution(* com.goldrootstock.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.goldrootstock.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	
+	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint joinPoint) {
+		
+		// display method we are calling
+		String theMethod = joinPoint.getSignature().toShortString();
+		myLogger.info("======> in @Before: calling method: " + theMethod);
+		
+		// display the arguments to the method
+		
+		// get the arguments
+		Object[] args = joinPoint.getArgs();
+		
+		// loop thru and display args 
+		for(Object tempArg : args) {
+			myLogger.info("======>> argument: " + tempArg);
+		}
+	}
+	
+	
+	// add @AfterReturning advice
+	@AfterReturning(
+			pointcut = "forAppFlow()",
+			returning = "theResult"
+			)
+	public void afterReturning(JoinPoint joinPoint, Object theResult) {
+		
+		// display method we are returning from
+		String theMethod = joinPoint.getSignature().toShortString();
+		myLogger.info("======> in @AfterReturning: from method: " + theMethod);
+		
+		
+		// display data returned 
+		myLogger.info("=====>> result: " + theResult);
+		
+	}
+	
+	
+
+}
